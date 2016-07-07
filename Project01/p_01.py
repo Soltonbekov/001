c79fff228816ccb389ac08095a87275afed4c454
Altynbek <altynbek@whatever.com>
2016-07-07 15:28:40 +0600

    Commit with changes on 07.07.2016

--- Project01/p_01.py
+++ Project01/p_01.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Row 1 07.07.2016
+print u"Hello world!"
+print u"Это проект 01"
+
+def func1():
+    pass
--- Project02/p_02.py
+++ Project02/p_02.py
@@ -1,3 +1,5 @@
 # coding: utf-8
 
 # First raw in second file 07.07.2016
+print u"Всем привет!"
+print u"This is project 2"
